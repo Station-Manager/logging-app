efdc6bca6bbe73d854d98ef8a66be5b6cb1283f1
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2025-11-26 13:24:58 +0200

    Add `QsoPanel` and `InfoPanel` components, integrate into `LoggingCardContent`
    Introduced `QsoPanel` and `InfoPanel` components with basic layouts and integrated them into the `LoggingCardContent` structure for improved modularity. Adjusted component styling to match dimensions.

--- frontend/src/lib/components/logging/LoggingCardContent.svelte
+++ frontend/src/lib/components/logging/LoggingCardContent.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
-
+    import QsoPanel from "$lib/components/logging/QsoPanel.svelte";
+    import InfoPanel from "$lib/components/logging/InfoPanel.svelte";
 </script>
 
-<div>
-Logging Card Content
+<div class="w-[1024px] h-[651px]">
+    <QsoPanel/>
+    <InfoPanel/>
 </div>
