e5e4b31c15c1107cc7589dfc2701e2e6d4b0ea30
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2025-12-04 14:14:56 +0200

    Add QSO Timer utilities and integrate start/stop controls in `TimerControls`
    - Introduced `startQsoTimer`, `stopQsoTimer`, and `isTimerRunning` utilities for managing QSO timers.
    - Added `onclick` events to timer control buttons for start/stop functionality.
    - Updated state and UI logic to disable buttons based on the timer's running state.

--- frontend/src/lib/ui/logging/components/TimerControls.svelte
+++ frontend/src/lib/ui/logging/components/TimerControls.svelte
@@ -1,18 +1,22 @@
 <script lang="ts">
 
+    import {isTimerRunning, startQsoTimer, stopQsoTimer} from "$lib/utils/qso-timer.svelte";
 </script>
 
 <div class="flex gap-x-1 w-[54px]">
     <button
             aria-label="stop"
             class="cursor-pointer disabled:text-gray-400 disabled:cursor-default enabled:text-red-500"
-            title="F5: Stop the QSO Timer">
+            title="F5: Stop the QSO Timer"
+            onclick={stopQsoTimer}
+            disabled={!isTimerRunning()}>
         <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round"
                   d="M5.25 7.5A2.25 2.25 0 0 1 7.5 5.25h9a2.25 2.25 0 0 1 2.25 2.25v9a2.25 2.25 0 0 1-2.25 2.25h-9a2.25 2.25 0 0 1-2.25-2.25v-9Z"/>
         </svg>
     </button>
     <button
+            onclick={startQsoTimer}
             aria-label="start"
             class="mr-8 cursor-pointer disabled:text-gray-400 disabled:cursor-default enabled:text-green-700"
             title="F5: Start the QSO Timer">
