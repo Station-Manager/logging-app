9ce8b3ef259f03e19778a874fe07b728e0412ac3
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2026-03-01 15:55:03 +0200

    Enhance `SetupPage` with logbook interface, state binding, and improved form structure.

--- frontend/src/lib/ui/setup/SetupPage.svelte
+++ frontend/src/lib/ui/setup/SetupPage.svelte
@@ -1,23 +1,63 @@
 <script lang="ts">
+    import {inputBase, inputBaseUppercase, labelBase} from "@station-manager/shared-utils";
+
+    interface Logbook {
+        name: string;
+        callsign: string;
+        description: string;
+    }
+
+    let logbook: Logbook = $state({
+        name: "Default",
+        callsign: "",
+        description: "",
+    });
 
 </script>
 
-<div>
-    <div class="mx-6">
+<div class="mx-6 flex flex-col gap-4">
+    <div>
         <h2 class="text-center">Setting your Default Logbook</h2>
         <p>This page will create the default logbook used by the Station Manager logging application. The callsign
-            you provided here will identify <b>Station Callsign</b> for all QSOs associated with this log book.
+            you provided here will identify the <b>Station Callsign</b> for all QSOs associated with this log book.
         </p>
-        <p>If you use QRZ.com, then the callsign should be the same as the callsign for the QRZ.com log book and API key
-            you are using.
+        <p>If you use QRZ.com, then the callsign entered here should be the same as the callsign for the QRZ.com log book
+            to which the QSOs will be forwarded (forwarding of QSOs is configurable and not enabled by default).
         </p>
     </div>
     <div>
-        <div>Name</div>
-        <div>Default</div>
+        <div class="flex flex-col w-37.5">
+            <label for="name">Name</label>
+            <input
+                    bind:value={logbook.name}
+                    id="name"
+                    type="text"
+                    class={inputBase}
+                    disabled>
+        </div>
     </div>
     <div>
-        <label for="callsign">Callsign</label>
-        <input id="callsign" type="text" placeholder="Callsign" />
+        <div class="flex flex-col w-37.5">
+            <label class={labelBase} for="callsign">Callsign</label>
+            <input
+                    bind:value={logbook.callsign}
+                    id="callsign"
+                    type="text"
+                    placeholder="Callsign"
+                    class={inputBaseUppercase}
+            />
+        </div>
+    </div>
+    <div>
+        <div class="flex flex-col w-48">
+            <label class={labelBase} for="description">Description</label>
+            <input
+                    bind:value={logbook.description}
+                    id="description"
+                    type="text"
+                    placeholder="Description"
+                    class={inputBase}
+            />
+        </div>
     </div>
 </div>
