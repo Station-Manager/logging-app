ed7876b58a975c57d64730c184a4f5e7b319e422
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2026-03-03 08:18:33 +0200

    Bump frontend and backend dependencies, including `@csstools/css-syntax-patches-for-csstree`, `flatted`, `postcss`, `tldts`, and `github.com/samber/lo`. Enhance `SetupPage` with improved layout, new `Save` button, and additional logbook field attributes.

--- frontend/src/lib/ui/setup/SetupPage.svelte
+++ frontend/src/lib/ui/setup/SetupPage.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import {inputBase, inputBaseUppercase, labelBase} from "@station-manager/shared-utils";
+    import {inputBase, inputBaseUppercase, inputCompact, labelBase} from "@station-manager/shared-utils";
 
     interface Logbook {
         name: string;
@@ -13,51 +13,62 @@
         description: "",
     });
 
+    const onClickSave = () => {
+        console.log("Save button clicked");
+    }
 </script>
 
 <div class="mx-6 flex flex-col gap-4">
     <div>
         <h2 class="text-center">Setting your Default Logbook</h2>
-        <p>This page will create the default logbook used by the Station Manager logging application. The callsign
-            you provided here will identify the <b>Station Callsign</b> for all QSOs associated with this log book.
+        <p>This page will create the <b>default logbook</b> used by the <i>Station Manager</i> logging application. The callsign
+            you provide here will identify the <i>Station Callsign</i> for all QSOs associated with this log book.
         </p>
         <p>If you use QRZ.com, then the callsign entered here should be the same as the callsign for the QRZ.com log book
             to which the QSOs will be forwarded (forwarding of QSOs is configurable and not enabled by default).
         </p>
     </div>
-    <div>
-        <div class="flex flex-col w-37.5">
-            <label for="name">Name</label>
-            <input
-                    bind:value={logbook.name}
-                    id="name"
-                    type="text"
-                    class={inputBase}
-                    disabled>
+    <div class="flex flex-col gap-y-4 mx-8">
+        <div>
+            <div class="flex flex-col w-37.5">
+                <label for="name" class={labelBase}>Name</label>
+                <input
+                        bind:value={logbook.name}
+                        id="name"
+                        type="text"
+                        class="{inputBase} text-gray-400"
+                        title="This cannot be changed."
+                        disabled>
+            </div>
         </div>
-    </div>
-    <div>
-        <div class="flex flex-col w-37.5">
-            <label class={labelBase} for="callsign">Callsign</label>
-            <input
-                    bind:value={logbook.callsign}
-                    id="callsign"
-                    type="text"
-                    placeholder="Callsign"
-                    class={inputBaseUppercase}
-            />
+        <div>
+            <div class="flex flex-col w-37.5">
+                <label class={labelBase} for="callsign">Callsign</label>
+                <input
+                        bind:value={logbook.callsign}
+                        id="callsign"
+                        type="text"
+                        placeholder="Callsign"
+                        class={inputBaseUppercase}
+                />
+            </div>
         </div>
-    </div>
-    <div>
-        <div class="flex flex-col w-48">
-            <label class={labelBase} for="description">Description</label>
-            <input
-                    bind:value={logbook.description}
-                    id="description"
-                    type="text"
-                    placeholder="Description"
-                    class={inputBase}
-            />
+        <div>
+            <div class="flex flex-col w-64">
+                <label class={labelBase} for="description">Description</label>
+                <input
+                        bind:value={logbook.description}
+                        id="description"
+                        type="text"
+                        placeholder="Description"
+                        class={inputBase}
+                />
+            </div>
+        </div>
+        <div class="flex justify-end">
+            <button
+                    onclick={onClickSave}
+                    class="h-9 cursor-pointer rounded-md bg-indigo-600 p-2.5 py-1.5 text-base font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 w-20">Save</button>
         </div>
     </div>
 </div>
--- go.mod
+++ go.mod
@@ -61,7 +61,7 @@ require (
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/rs/zerolog v1.34.0 // indirect
-	github.com/samber/lo v1.52.0 // indirect
+	github.com/samber/lo v1.53.0 // indirect
 	github.com/spf13/cast v1.10.0 // indirect
 	github.com/tkrajina/go-reflector v0.5.8 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
